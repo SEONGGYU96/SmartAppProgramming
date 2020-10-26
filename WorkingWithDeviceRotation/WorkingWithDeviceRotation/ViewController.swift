ed129ff08bab0e7eebdc2c2e38f099ecd600db4f
김성규 <link5658@gmail.com>
2020-10-26 20:31:00 +0900

    Add working with rotation app

---
+++ WorkingWithDeviceRotation/WorkingWithDeviceRotation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingWithDeviceRotation
+//
+//  Created by 김성규 on 2020/10/26.
+//  Copyright © 2020 KIM SEONGGYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
